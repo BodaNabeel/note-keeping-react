6708a3750307cc9e4dd9252e85440afcf443478a
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-03 13:55:37 +0530

    sample notes added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,37 @@ import { useState } from "react";
 import NoteList from "./components/NoteList";
 
 function App() {
-  const [note, setNote] = useState(null);
+  const [note, setNote] = useState([
+    {
+      noteInfo:
+        "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
+      noteID: 1,
+    },
+    {
+      noteInfo:
+        "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
+      noteID: 2,
+    },
+    {
+      noteInfo:
+        "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
+      noteID: 3,
+    },
+    {
+      noteInfo:
+        "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
+      noteID: 4,
+    },
+    {
+      noteInfo:
+        "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
+      noteID: 5,
+    },
+  ]);
   return (
-   <>
-  <NoteList/>
-   </>
+    <>
+      <NoteList/>
+    </>
   );
 }
 
