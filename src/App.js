2bc3e264eb9ccac67e9793dcf0f1d66294e53fe6
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-03 15:53:34 +0530

    uuid implemented

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import { useState } from "react";
 import NoteList from "./components/NoteList";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [note, setNote] = useState([
     {
-      text:
-        "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      noteID: 1,
+      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
+      id: uuidv4(),
     },
     {
-      text:
-        "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      noteID: 2,
+      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
+      id: uuidv4(),
     },
     {
-      text:
-        "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      noteID: 3,
+      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
+      id: uuidv4(),
     },
     {
-      text:
-        "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      noteID: 4,
+      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
+      id: uuidv4(),
     },
     {
-      text:
-        "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      noteID: 5,
+      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
+      id: uuidv4(),
     },
   ]);
   return (
     <>
-      <NoteList note={note}/>
+      <NoteList note={note} />
     </>
   );
 }
