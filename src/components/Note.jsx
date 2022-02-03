0a418df55e7d4bba14e5b0ed10c790e1e198d3dd
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-03 15:53:43 +0530

    uuid implemented

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-
 export default function Note({ id, text }) {
   return (
     <>
-      <div className="note">
-        <p className="note-text">{text}</p>
-        <button>del</button>
+      <div className="note" key={id} >
+        <p className="note-text" id={id}>{text}</p>
+        <button className="note-btn" id={id}>del</button>
       </div>
     </>
   );
