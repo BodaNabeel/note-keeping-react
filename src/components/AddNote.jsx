27cc83a7faa100c477089429d226c1dd22ba4b12
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-04 16:24:54 +0530

    fixed the issue with addnotes values

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -3,20 +3,25 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function AddNote({ notes, setNotes }) {
   const textarea = useRef();
+  const [text, setText] = useState();
+  const getValue = (e) => {
+    setText(e.target.value);
+  };
   const handleAdd = () => {
-      if(textarea.current.value.trim().length > 0) {
-          setNotes((prevState) => {
-              return [
-              ...prevState,
-              {
-                  text: textarea.current.value,
-                  id: uuidv4()
-              }
-          ]})
-          textarea.current.value = ""
-      } else {
-          alert("note can't be blank")
-      }
+    if (textarea.current.value.trim().length > 0) {
+      setNotes((prevState) => {
+        return [
+          ...prevState,
+          {
+            text: text,
+            id: uuidv4(),
+          },
+        ];
+      });
+      textarea.current.value = "";
+    } else {
+      alert("note can't be blank");
+    }
   };
   return (
     <>
@@ -24,10 +29,11 @@ export default function AddNote({ notes, setNotes }) {
         <textarea
           ref={textarea}
           className="add-note_textarea"
-          name=""
-          id=""
           cols="30"
           rows="10"
+          onChange={(e) => {
+            getValue(e);
+          }}
           placeholder="enter new note..."
           maxLength={200}
         ></textarea>
