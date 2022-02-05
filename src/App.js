829788c74d1dca8f486e24b3bd22d11df087d899
Nabeel Boda <needtoexplore01@gmail.com>
2022-02-05 20:47:11 +0530

    localStorage added

--- src/App.js
+++ src/App.js
@@ -1,53 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NoteList from "./components/NoteList";
 import { v4 as uuidv4 } from "uuid";
 import Search from "./components/Search";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
+  const initalValue = [
+    {
+      text: "It's a dummy note",
+      id: uuidv4(),
+    },
+  ];
+  const [notes, setNotes] = useLocalStorage("notes", initalValue);
   const [searchText, setSearchText] = useState("");
-  const [notes, setNotes] = useState([
-    {
-      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      id: uuidv4(),
-    },
-    {
-      text: "i'm learning re!",
-      id: uuidv4(),
-    },
-    {
-      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      id: uuidv4(),
-    },
-    {
-      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      id: uuidv4(),
-    },
-    {
-      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      id: uuidv4(),
-    },
-    {
-      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      id: uuidv4(),
-    },
-    {
-      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      id: uuidv4(),
-    },
-    {
-      text: "i'm learning reactjs and i's really fun and i'm enjoying the process on the way yooo!!!",
-      id: uuidv4(),
-    },
-  ]);
+
   return (
     <>
       <div className="flex-wrapper">
         <div className="main-content">
           <Search setSearchText={setSearchText} />
           <NoteList
-            notes={notes.filter((notes) =>
-              notes.text.toLowerCase().includes(searchText.toLowerCase())
-            )}
+            notes={
+              notes.length > 0
+                ? notes.filter((notes) =>
+                    notes.text.toLowerCase().includes(searchText.toLowerCase())
+                  )
+                : []
+            }
             setNotes={setNotes}
           />
         </div>
